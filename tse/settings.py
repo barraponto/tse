c631b79ef6ef0a98b9e66e6c619948cc2379b53a
Capi Etheriel <barraponto@gmail.com>
2014-08-10 14:20:08 -0300

    fixed settings to store files under project root

--- tse/settings.py
+++ tse/settings.py
@@ -1,19 +1,14 @@
 # -*- coding: utf-8 -*-
+import os.path
 
-# Scrapy settings for tse project
-#
-# For simplicity, this file contains only the most important settings by
-# default. All the other settings are documented here:
-#
-#     http://doc.scrapy.org/en/latest/topics/settings.html
-#
 
 BOT_NAME = 'tse'
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 SPIDER_MODULES = ['tse.spiders']
 NEWSPIDER_MODULE = 'tse.spiders'
 ITEM_PIPELINES = {'tse.pipelines.StatePipeline'}
-FILES_STORE = 'files'
+FILES_STORE = os.path.join(PROJECT_ROOT, 'files')
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'tse (+http://www.yourdomain.com)'
