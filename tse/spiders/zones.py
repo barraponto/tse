a6c7146020a0a1a7424fb805d332444eaad24036
Capi Etheriel <barraponto@gmail.com>
2014-08-10 04:03:15 -0300

    Getting relevant data in zones spider

---
+++ tse/spiders/zones.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from urlparse import urljoin
+import scrapy
+from ..items import ZoneItem
+
+class ZonesSpider(scrapy.Spider):
+    name = "zones"
+    allowed_domains = ["estatistica.tse.jus.br"]
+    start_urls = (
+        'http://estatistica.tse.jus.br:7777/dwtse/f?p=600:',
+    )
+
+    def parse(self, response):
+        # Just getting cookies the first time around.
+        return (scrapy.Request(self.start_urls[0] + str(n), self.parse_state)
+                for n in xrange(1, 29))
+
+    def parse_state(self, response):
+        item = ZoneItem()
+        item['state'] = response.css(
+            '.t15RegionHeader::text').extract()[0].split('-')[-1]
+        item['excel'] = urljoin(
+            response.url,
+            response.css('span.left a::attr(href)').extract()[0])
+        return item
